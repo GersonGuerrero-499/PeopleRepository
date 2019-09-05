b2317807b25a27ea118d8a9f7c05c303c0138252
@CDS2UNIVO18 <CDS2UNIVO10@cds2univo18>
2019-09-05 08:39:14 -0600

    Model Class Person added

---
+++ App1/App1/App1/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace App1.Model
+{
+    [Table("T_People")]
+    public class Person
+    {
+        //miembro properties
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; } 
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+    }
+}
